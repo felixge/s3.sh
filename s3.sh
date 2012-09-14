c226f212ffe865ed4db6daa1d912ef8a1d30546f
Felix Geisendörfer <felix@debuggable.com>
2012-09-14 13:47:54 +0200

    Add s3.sh

---
+++ s3.sh
@@ -0,0 +1,25 @@
+s3_sign_url() {
+  local awsKey=${1}
+  local awsSecret=${2}
+  local bucket=${3}
+  local path=${4}
+  # Unix epoch number, defaults to 60 seconds in the future
+  local expires=${5:-`date -v+60S +%s`}
+
+  local stringToSign="GET\n\n\n${expires}\n/${bucket}/${path}"
+
+  local base64Signature=`echo -en "${stringToSign}" |\
+    openssl dgst -sha1 -binary -hmac ${awsSecret} |\
+    openssl base64`
+
+  # Escape all base64 special characters ('+', '=', '\') for url
+  local escapedSignature=${base64Signature}
+  escapedSignature=${escapedSignature//+/%2b}
+  escapedSignature=${escapedSignature//\//%2f}
+  escapedSignature=${escapedSignature//=/%3d}
+
+  local query="Expires=${expires}&AWSAccessKeyId=${awsKey}&Signature=${escapedSignature}"
+  local url="http://s3.amazonaws.com/${bucket}/${path}?${query}"
+
+  echo ${url}
+}
