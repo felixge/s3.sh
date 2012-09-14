036e298ab75e3c7d4402cd85aa8a8d460a9d90ed
Felix Geisendörfer <felix@debuggable.com>
2012-09-14 14:09:39 +0200

    Proper urlencode, add ${httpMethod} parameter

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Bash functions for Amazon S3. (Not complete, just scratching my itch)
 # API
 
 ```bash
-s3_sign_url ${awsKey} ${awsSecret} ${bucket} ${path} ${expires:-`date -v+60S +%s`}
+s3_sign_url ${httpMethod} ${bucket} ${path} ${awsKey} ${awsSecret} ${expires:-`date -v+60S +%s`}
 ```
 
 Returns an url suitable for making a GET request to a private s3 object.
--- s3.sh
+++ s3.sh
@@ -1,27 +1,45 @@
 # send patches to: https://github.com/felixge/s3.sh
 
 s3_sign_url() {
-  local awsKey=${1}
-  local awsSecret=${2}
-  local bucket=${3}
-  local path=${4}
+  local httpMethod=${1}
+  local bucket=${2}
+  local path=${3}
+  local awsKey=${4}
+  local awsSecret=${5}
   # Unix epoch number, defaults to 60 seconds in the future
-  local expires=${5:-`date -v+60S +%s`}
+  local expires=${6:-`date -v+60S +%s`}
 
-  local stringToSign="GET\n\n\n${expires}\n/${bucket}/${path}"
+  local stringToSign="${httpMethod}\n\n\n${expires}\n/${bucket}/${path}"
 
   local base64Signature=`echo -en "${stringToSign}" \
     | openssl dgst -sha1 -binary -hmac ${awsSecret} \
     | openssl base64`
 
-  # Escape all base64 special characters ('+', '=', '\') for url
-  local escapedSignature=${base64Signature}
-  escapedSignature=${escapedSignature//+/%2b}
-  escapedSignature=${escapedSignature//\//%2f}
-  escapedSignature=${escapedSignature//=/%3d}
+  local escapedSignature=`_s3_urlencode ${base64Signature}`
+  local escapedAwsKey=`_s3_urlencode ${awsKey}`
 
-  local query="Expires=${expires}&AWSAccessKeyId=${awsKey}&Signature=${escapedSignature}"
+  local query="Expires=${expires}&AWSAccessKeyId=${escapedAwsKey}&Signature=${escapedSignature}"
   local url="http://s3.amazonaws.com/${bucket}/${path}?${query}"
 
   echo ${url}
+}
+
+# from: http://ethertubes.com/bash-snippet-url-encoding/
+_s3_urlencode () {
+  local tab="`echo -en "\x9"`"
+  local i="$@"
+
+  i=${i//%/%25}  ; i=${i//' '/%20} ; i=${i//$tab/%09}
+  i=${i//!/%21}  ; i=${i//\"/%22}  ; i=${i//#/%23}
+  i=${i//\$/%24} ; i=${i//\&/%26}  ; i=${i//\'/%27}
+  i=${i//(/%28}  ; i=${i//)/%29}   ; i=${i//\*/%2a}
+  i=${i//+/%2b}  ; i=${i//,/%2c}   ; i=${i//-/%2d}
+  i=${i//\./%2e} ; i=${i//\//%2f}  ; i=${i//:/%3a}
+  i=${i//;/%3b}  ; i=${i//</%3c}   ; i=${i//=/%3d}
+  i=${i//>/%3e}  ; i=${i//\?/%3f}  ; i=${i//@/%40}
+  i=${i//\[/%5b} ; i=${i//\\/%5c}  ; i=${i//\]/%5d}
+  i=${i//\^/%5e} ; i=${i//_/%5f}   ; i=${i//\`/%60}
+  i=${i//\{/%7b} ; i=${i//|/%7c}   ; i=${i//\}/%7d}
+  i=${i//\~/%7e}
+  echo "$i"
 }
